6c40d4cf2d7fd8c39410613be0c24a9998c170c3
Mathias Bolle <mbolle2310@protonmail.com>
2024-01-21 07:04:19 -0800

    fix: first puzzle

---
+++ src/main/kotlin/day1/Main.kt
@@ -0,0 +1,39 @@
+package day1
+
+import java.io.File
+
+fun main() {
+    val encodedCalibrations = File("src/main/resources/1.txt").readLines()
+
+    val solution = sumOfCalibrationValues(encodedCalibrations)
+
+    println("The sum of the all the calibration values are:")
+    println(solution)
+}
+
+fun sumOfCalibrationValues(encodedCalibration: List<String>): Int {
+    var calibrationsSum = 0
+
+    for (encodedCalibrationLine in encodedCalibration) {
+
+        var calibrationNumber = ""
+        val codeToChar = encodedCalibrationLine.toCharArray()
+
+        for (char in codeToChar) {
+            if (char.digitToIntOrNull() != null) {
+                calibrationNumber += char
+                break
+            }
+        }
+
+        for (charIndexed in codeToChar.size - 1 downTo 0) {
+            if (codeToChar[charIndexed].digitToIntOrNull() != null) {
+                calibrationNumber += codeToChar[charIndexed]
+                break
+            }
+        }
+
+        calibrationsSum += (calibrationNumber.toInt())
+    }
+    return calibrationsSum
+}
